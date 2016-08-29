d2c1fe0e079c48942dc60a2dcde16662f8c47d2d
binbinxiao <460116602@qq.com>
2016-08-29 21:52:39 +0800

    第二次上传

--- app/src/main/java/com/sanrenzu/chunbo/fragment/FenleiFragment.java
+++ app/src/main/java/com/sanrenzu/chunbo/fragment/FenleiFragment.java
@@ -1,9 +1,16 @@
 package com.sanrenzu.chunbo.fragment;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 
 /**
  * Created by My on 2016/8/29.
  */
 public class FenleiFragment extends Fragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
 }
---
+++ app/src/main/java/com/sanrenzu/chunbo/helper/ceshi.java
@@ -0,0 +1,8 @@
+package com.sanrenzu.chunbo.helper;
+
+/**
+ * Created by My on 2016/8/29.
+ */
+public class ceshi {
+
+}
